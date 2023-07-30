9cfba363013febedfac6e078469f6dd92d5bd9f7
Gabriel Machado <bieloliveiramachado@gmail.com>
2023-07-30 15:38:53 -0300

    Category

--- src/main/java/com/gabriel/course/config/TesteConfig.java
+++ src/main/java/com/gabriel/course/config/TesteConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.gabriel.course.entities.Category;
 import com.gabriel.course.entities.Order;
 import com.gabriel.course.entities.User;
 import com.gabriel.course.entities.enums.OrderStatus;
+import com.gabriel.course.repositories.CategoryRepository;
 import com.gabriel.course.repositories.OrderRepository;
 import com.gabriel.course.repositories.UserRepository;
 
@@ -23,9 +25,17 @@ public class TesteConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category c1 = new Category(null, "Books");
+		Category c2 = new Category(null, "Eletronics");
+		Category c3 = new Category(null, "Shoes");
+		
 		User user1 = new User(null, "Maria Silva", "maria@yahoo.com", "1199997778", "abc");
 		User user2 = new User(null, "Jose Almeida", "jose@hotmail.com", "3198877667", "1234");
 		
@@ -33,6 +43,7 @@ public class TesteConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2023-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
 		Order o3 = new Order(null, Instant.parse("2023-07-22T15:21:22Z"), OrderStatus.DELIVERED, user1);
 		
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
---
+++ src/main/java/com/gabriel/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.gabriel.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabriel.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{	
+
+	
+}
